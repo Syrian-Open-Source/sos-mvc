05e3d3689176eef7a1c60648fa69366cc05f10e2
karam  mustafa <karam2mustafa@gmail.com>
2021-10-07 14:06:46 +0300

    update encapsulate

--- controllers/ContactController.php
+++ controllers/ContactController.php
@@ -4,10 +4,13 @@
 namespace app\controllers;
 
 
+use app\core\Application;
+
 class ContactController
 {
     public function index()
     {
+        return Application::$instance->router->renderView('contact');
 
     }
 }
