78f7e5aefdfd87acd9a8d716c4134d350633a11f
karam  mustafa <karam2mustafa@gmail.com>
2021-10-07 14:25:19 +0300

    add contact route link

--- views/layoutes/main.php
+++ views/layoutes/main.php
@@ -24,6 +24,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/about">About</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contact">Contact</a>
+                </li>
             </ul>
         </div>
     </div>
