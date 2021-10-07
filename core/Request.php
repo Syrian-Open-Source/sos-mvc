047a69ca0a714dc9a4c037a1f03e6afb50e6c9d8
karam  mustafa <karam2mustafa@gmail.com>
2021-10-07 14:56:32 +0300

    update resolve request body

--- core/Request.php
+++ core/Request.php
@@ -7,6 +7,18 @@ namespace app\core;
 class Request
 {
 
+    private $availableMethods = [
+        'get' => [INPUT_GET],
+        'post' => [INPUT_POST],
+    ];
+
+
+    /**
+     * get the request path without query params.
+     *
+     * @return false|mixed|string
+     * @author karam mustafa
+     */
     public function getPath()
     {
         $path = $_SERVER['REQUEST_URI'] ?? '/';
@@ -18,8 +30,49 @@ class Request
             : $path;
     }
 
+    /**
+     * get the method name.
+     *
+     * @return string
+     * @author karam mustafa
+     */
     public function getMethod()
     {
         return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+
+    /**
+     * get the body of get request, this will filter the inputs and return the secure body.
+     *
+     * @return false|mixed|string
+     * @author karam mustafa
+     */
+    public function getBody()
+    {
+        $body = [];
+        $request = $this->resolveMethodType();
+        foreach ($request[0] as $key => $value) {
+            $body[$key] = filter_input($request[1], $key, FILTER_SANITIZE_SPECIAL_CHARS);
+        }
+
+        return $body;
+    }
+
+    /**
+     * this will match the method of request, and get the variable to make the process dynamic.
+     *
+     * @return false|mixed|string
+     * @author karam mustafa
+     */
+    private function resolveMethodType()
+    {
+        $type = $_GET;
+
+        if ($this->getMethod() == 'post') {
+            $type = $_POST;
+        }
+
+        return [$type, $this->availableMethods[$this->getMethod()][0]];
+
     }
 }
