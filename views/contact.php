7d0bc9288d24aa5a277e3f74b42f5a5468127c53
karam  mustafa <karam2mustafa@gmail.com>
2021-10-07 14:20:58 +0300

    update contact from

--- views/contact.php
+++ views/contact.php
@@ -1,8 +1,16 @@
-<div class="mb-3">
-    <label for="exampleFormControlInput1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-</div>
-<div class="mb-3">
-    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
+<form action="/contact" method="post">
+    <h1>form title is: <?php echo $title; ?></h1>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">First name</label>
+        <input type="text" name="first_name" class="form-control">
+    </div>
+
+    <div class="mb-3">
+        <label class="form-label">Last name</label>
+        <input type="text" name="last_name" class="form-control">
+    </div>
+    <div class="col-auto">
+        <button type="submit" class="btn btn-primary mb-3">Submit</button>
+    </div>
+
+</form>
