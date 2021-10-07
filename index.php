0a7eef1ada9d693d5b60aa1ae69cf9828f3ab4b8
karam  mustafa <karam2mustafa@gmail.com>
2021-10-07 14:03:38 +0300

    refactor to bind controllers

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 
 use app\core\Application;
+use app\controllers\HomeController;
+use app\controllers\AboutController;
+use app\controllers\ContactController;
 
 require_once __DIR__.'./vendor/autoload.php';
 
 $app = new Application(basename(__DIR__));
 
-$app->router->get('/home', 'home');
-$app->router->post('/about', 'about');
+$app->router->get('/home', [new HomeController(), 'index']);
+$app->router->get('/about', [new AboutController(), 'index']);
+$app->router->get('/contact', [new ContactController(), 'index']);
 
 $app->run();
