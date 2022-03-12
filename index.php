42635d45d6ca4911b97192ad38b9041cbcd08067
karam  mustafa <karam2mustafa@gmail.com>
2022-03-12 15:47:50 +0200

    wip

--- index.php
+++ index.php
@@ -1,24 +1,11 @@
 <?php
 
-use app\controllers\AuthController;
 use app\core\Application;
-use app\controllers\HomeController;
-use app\controllers\AboutController;
-use app\controllers\ContactController;
+use app\core\Config;
 
 require_once __DIR__.'./vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 
-$config = [
-  'db' => [
-      'dsn' => $_ENV['DB_DSN'],
-      'user' => $_ENV['DB_USERNAME'],
-      'password' => $_ENV['DB_PASSWORD'],
-  ]
-];
-
-$app = new Application(basename(__DIR__) , $config);
+$app = new Application(basename(__DIR__) , (new Config())->getEnv());
 
 $app->run();
