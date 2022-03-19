b0b07ee1dc13c443bec0df05f5f2213917d7b00f
karam  mustafa <karam2mustafa@gmail.com>
2022-03-19 18:42:42 +0200

    wip in application events

--- core/Application.php
+++ core/Application.php
@@ -121,9 +121,9 @@ class Application
         $this->response = new Response();
         $this->session = new Session();
         $this->view = new View();
+        $this->events = new Events();
         $this->router = new Router($this->request, $this->response);
         $this->db = new Database($config['db']);
-        $this->events = new Events();
     }
 
     /**
