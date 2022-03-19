b6dc238deb6a1f2f13cb64bd2fb5f3b3e2f99ee7
karam  mustafa <karam2mustafa@gmail.com>
2022-03-20 00:35:15 +0200

    wip in active record

--- core/ActiveRecord/Connection.php
+++ core/ActiveRecord/Connection.php
@@ -4,7 +4,30 @@
 namespace app\core\ActiveRecord;
 
 
+use GuzzleHttp\Client;
+
 class Connection
 {
+    /**
+     * The HTTP Client
+     *
+     * @var Client
+     */
+    protected Client $client;
 
+    public function client()
+    {
+        if ($this->client) return $this->client;
+
+        return new Client([
+            'base_url' => "ANY",
+            'defaults' => [
+                'auth' => [$this->key, 'x', 'basic'],
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json'
+                ]
+            ]
+        ]);
+    }
 }
