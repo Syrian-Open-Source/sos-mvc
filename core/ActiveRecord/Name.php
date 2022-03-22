35ec1433895c3a9255248f7083831009114b8e41
karam  mustafa <karam2mustafa@gmail.com>
2022-03-22 23:43:10 +0200

    wip

--- core/ActiveRecord/Name.php
+++ core/ActiveRecord/Name.php
@@ -6,5 +6,22 @@ namespace app\core\ActiveRecord;
 
 class Name
 {
+    /**
+     * The name
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Create a new Name instance
+     *
+     * @param string $name
+     * @return void
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
 
 }
