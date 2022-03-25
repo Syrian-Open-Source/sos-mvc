59fa426bd81325ea4a85f27d65f79e3d29298ce0
karam  mustafa <karam2mustafa@gmail.com>
2022-03-25 21:29:34 +0300

    wip

--- core/ActiveRecord/Model.php
+++ core/ActiveRecord/Model.php
@@ -41,7 +41,7 @@ abstract class Model
     /**
      * Get the connection instance
      *
-     * @return \app\core\ActiveRecord\Connection
+     * @return Connection
      * @author karam mustafa
      */
     public function connection()
@@ -55,11 +55,13 @@ abstract class Model
      * @param  string  $key
      * @param  mixed  $value
      *
-     * @return void
+     * @return Model
      */
     protected function setAttribute($key, $value)
     {
         $this->attributes[$key] = $value;
+
+        return $this;
     }
 
     /**
@@ -86,6 +88,7 @@ abstract class Model
      * @param $key
      * @param $value
      *
+     * @return Model
      * @throws \Exception
      * @author karam mustafa
      */
