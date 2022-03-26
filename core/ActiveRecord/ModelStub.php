1273c9c2019ab39eca4d5c654d94bbb4468a49f8
karam  mustafa <karam2mustafa@gmail.com>
2022-03-26 23:56:13 +0300

    wip

--- core/ActiveRecord/ModelStub.php
+++ core/ActiveRecord/ModelStub.php
@@ -6,5 +6,12 @@ namespace app\core\ActiveRecord;
 
 class ModelStub extends Model
 {
+    protected array $fillable = ["name", "email"];
 
+    public function __construct(Connection $connection, $attributes = [])
+    {
+        parent::__construct($connection);
+
+        $this->fill($attributes);
+    }
 }
